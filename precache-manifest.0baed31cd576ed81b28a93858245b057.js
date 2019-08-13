c6fc4c60bc906c4f7930e16c535ab86ee78e3906
José Courcault <jcourcault@gmail.com>
2019-08-13 11:45:17 +0200

    Updates

---
+++ precache-manifest.0baed31cd576ed81b28a93858245b057.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d55f580170c666bcd0f79c4e89cbe1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1b15396ec7b82fba4db7",
+    "url": "/monsters-rolodex/static/css/main.2af9f48f.chunk.css"
+  },
+  {
+    "revision": "607e395712115653f754",
+    "url": "/monsters-rolodex/static/js/2.45dda9c7.chunk.js"
+  },
+  {
+    "revision": "1b15396ec7b82fba4db7",
+    "url": "/monsters-rolodex/static/js/main.e4ba09e7.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
